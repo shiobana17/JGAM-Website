5d4b76d5e436b4d3e837899a2f0b502aadc68c5f
shiobana <tejeshwara447@gmail.com>
2025-09-24 15:25:06 +0530

    Compressed image sizes

--- JGAM-Website/backend/app.py
+++ JGAM-Website/backend/app.py
@@ -44,5 +44,5 @@ def get_gallery_data() -> Response:
     return jsonify(albums)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
 
